068e1d5179650e0b252dd33a73bcc11d6d38b22f
mtchorowski <mtchorowski@artgroup.pl>
2018-12-23 19:03:15 +0100

    test'

---
+++ src/views/Search.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="wrapper">
+    <div class="search">
+      <label for="search">Search</label>
+      <input id="search" name="search" v-model="searchValue" @input="handleInput" />
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+const API = 'https://images-api.nasa.gov';
+export default {
+  name: 'Search',
+  data() {
+    return {
+      searchValue: '',
+    };
+  },
+  methods: {
+    handleInput() {
+      axios.get(`${API}/search?q=${this.searchValue}&media_type=image`)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  .wrapper {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 0;
+    padding: 30px;
+    width: 100%;
+  }
+
+  .search {
+    display: flex;
+    flex-direction: column;
+    width: 250px;
+    label {
+      font-family: Montserrat, sans-serif;
+    }
+    input {
+      height: 30px;
+      border: 0;
+      border-bottom: 1px solid black;
+    }
+  }
+</style>
